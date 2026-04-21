779bb4471599ebb4dddc8708659179e4c37efc92
golden-Puffle <monisha.agarwal09@gmail.com>
2026-04-21 16:27:27 +0530

    Phase 1: Implement object header construction in object_write

--- object.c
+++ object.c
@@ -94,9 +94,27 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
-    // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
+    const char *type_str;
+    if (type == OBJ_BLOB) type_str = "blob";
+    else if (type == OBJ_TREE) type_str = "tree";
+    else if (type == OBJ_COMMIT) type_str = "commit";
+    else return -1;
+
+    // Step 1: Build header
+    char header[64];
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len);
+    size_t full_len = header_len + 1 + len; // +1 for the '\0'
+    uint8_t *full = malloc(full_len);
+    if (!full) return -1;
+    memcpy(full, header, header_len);
+    full[header_len] = '\0';
+    memcpy(full + header_len + 1, data, len);
+
+    // TODO: hash, dedup, write (coming next)
+    free(full);
+    (void)id_out;
     return -1;
+}
 }
 
 // Read an object from the store.
